62a05f7217cb7957cd8d9638bc2e406baf225940
Luke <lukebmorgan@gmail.com>
2023-09-06 01:00:26 -0400

    end of phase 0

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
-
 require('express-async-errors');
-
 const morgan = require('morgan');
 const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+
 const { environment } = require('./config');
 const isProduction = environment === 'production';
+
+const routes = require('./routes');
 
 const app = express();
 
 app.use(morgan('dev'));
-
 app.use(cookieParser());
-
 app.use(express.json());
 
 // Security Middleware
@@ -40,3 +39,8 @@ app.use(
     }
   })
 );
+
+
+app.use(routes);
+
+module.exports = app;
