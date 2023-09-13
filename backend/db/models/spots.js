4955645855a7362af8f2f9ba38c5c7435b1b7af2
Luke <lukebmorgan@gmail.com>
2023-09-12 21:07:30 -0400

    add create spots

---
+++ backend/db/models/spots.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spots extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Spots.init({
+    ownerId:{
+      type: DataTypes.INTEGER,
+      allowNull : false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull : false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull : false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull : false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull : false
+    },
+    lat: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull : false
+    },
+    lng: {
+      type: DataTypes.DECIMAL(5, 2),
+      allowNull : false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull : false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull : false
+    },
+  }, {
+    sequelize,
+    modelName: 'Spots',
+  });
+  return Spots;
+};
